48cc4d772e593aa7f5178ee4af7161212543f640
wutsi <herve.tchepannou@wutsi.com>
2022-10-03 21:50:19 -0400

    conversationId provided by client

--- src/main/kotlin/com/wutsi/platform/chat/dto/SendMessageRequest.kt
+++ src/main/kotlin/com/wutsi/platform/chat/dto/SendMessageRequest.kt
@@ -4,6 +4,7 @@ import kotlin.Long
 import kotlin.String
 
 public data class SendMessageRequest(
+  public val conversationId: String = "",
   public val referenceId: String = "",
   public val recipientId: Long = 0,
   public val text: String = "",
